96d4f469e775d23de60579f672092ee93da6d40a
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2022-11-17 05:24:16 +0100

    Fix: Changefile extension to tsx, added type to props

--- src/components/HeadBtn/HeadBtn.tsx
+++ src/components/HeadBtn/HeadBtn.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const HeadBtn = ({ title }) => {
+const HeadBtn = ({ title }: {title: string}) => {
   return (
     <div className='mb-5 bg-[#F0EFEF] px-[6rem] rounded-full cursor-pointer '>
       <h1 className='text-[20px] text-center py-4 px-7 bg-[#FF4545] rounded-full font-bold'>{title}</h1>
