99edd5c5fc85f2f37c658f2b9e171b44bcf4bcea
Tk-brainCodes <ikuohoneboreimethankgod@gmail.com>
2022-11-19 09:09:04 +0100

    Feature: User signup asynchronous thunk

---
+++ src/redux/reducers/users/userAction.ts
@@ -0,0 +1,59 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { IValidateTypes } from "./userSlice";
+import setAuthToken from "../../../utils/authToken";
+import setBaseUrl from "../../../utils/baseUrl";
+
+interface Error {
+  response: any;
+  name: string;
+  message: string;
+  stack?: string;
+  status?: number;
+  code?: number;
+}
+
+export const signupUser = createAsyncThunk(
+  "user/register",
+  async ({ name, email, matNumber, password, confirmPassword }: IValidateTypes, thunkAPI) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const token = localStorage.getItem("token");
+    if (token) {
+      setAuthToken(token);
+    }
+
+    //dummy endpoint link
+    const url = "/register";
+
+    try {
+      const response = await setBaseUrl.post(
+        url,
+        { name, email, matNumber, password, confirmPassword },
+        config
+      );
+
+      let data = await response.data;
+      console.log(data);
+
+      if (response.status === 200) {
+        localStorage.setItem("token", data.token);
+        return {
+          ...data,
+          name: name,
+          email: email,
+          matNumber: matNumber,
+          password: password,
+        };
+      } else {
+        return thunkAPI.rejectWithValue(data);
+      }
+    } catch (error) {
+      console.log("Error", (error as Error).response.data);
+      return thunkAPI.rejectWithValue((error as Error).response.data);
+    }
+  }
+);
